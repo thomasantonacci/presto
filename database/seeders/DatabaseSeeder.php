1cc9928e0ec5c91ef13a0020951094c11e7b314a
alessandrocaporale <ale.caporale@outlook.it>
2025-05-22 21:38:10 +0200

    seeder articles aggiunto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use App\Models\User;
 use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -31,7 +32,7 @@ class DatabaseSeeder extends Seeder
       }
 
 
-      $users= [
+      $users = [
          [
             "name" => "Linda",
             "email" => "linda@mail.com",
@@ -97,8 +98,86 @@ class DatabaseSeeder extends Seeder
 
       ];
 
-      foreach ($users as $user){
+      foreach ($users as $user) {
          User::create($user);
+      }
+
+      $articles = [
+         [
+            "title" => "Smartphone Samsung Galaxy",
+            "description" => "Smartphone di ultima generazione con display AMOLED.",
+            "price" => 399.99,
+            "category_id" => 1,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Giacca invernale uomo",
+            "description" => "Giacca calda e resistente per l'inverno.",
+            "price" => 89.90,
+            "category_id" => 2,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Set skincare viso",
+            "description" => "Kit completo per la cura della pelle.",
+            "price" => 29.99,
+            "category_id" => 3,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Robot tagliaerba",
+            "description" => "Robot automatico per il taglio del prato.",
+            "price" => 499.00,
+            "category_id" => 4,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Pallone da calcio",
+            "description" => "Pallone professionale per allenamento e partita.",
+            "price" => 24.50,
+            "category_id" => 6,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Cuffie Bluetooth",
+            "description" => "Cuffie wireless con cancellazione del rumore.",
+            "price" => 59.99,
+            "category_id" => 1,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Scarpe da running",
+            "description" => "Scarpe leggere e traspiranti per la corsa.",
+            "price" => 74.90,
+            "category_id" => 2,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Crema idratante",
+            "description" => "Crema viso nutriente per tutti i tipi di pelle.",
+            "price" => 19.99,
+            "category_id" => 3,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Set attrezzi giardinaggio",
+            "description" => "Set completo di attrezzi per il giardinaggio.",
+            "price" => 34.50,
+            "category_id" => 4,
+            "user_id" => 1
+         ],
+         [
+            "title" => "Bicicletta da bambino",
+            "description" => "Bicicletta colorata con rotelle per bambini.",
+            "price" => 120.00,
+            "category_id" => 6,
+            "user_id" => 1
+         ]
+
+      ];
+
+      foreach ($articles as $article) {
+         Article::create($article);
       }
    }
 }
