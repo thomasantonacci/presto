fa33493e48ba06d20d3f148ca0c621903781a568
alessandrocaporale <ale.caporale@outlook.it>
2025-05-22 21:15:49 +0200

    user story 2 completata + seeder users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,30 +2,103 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use App\Models\User;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
- public $categories = [
-    'elettronica',
-    'abbigliamento',
-    'salute e bellezza',
-    'casa e giardinaggio',
-    'giocattoli',
-    'sport',
-    'animali domestici',
-    'libri e riviste',
-    'accessori',
-    'motori',
+   public $categories = [
+      'elettronica',
+      'abbigliamento',
+      'salute e bellezza',
+      'casa e giardinaggio',
+      'giocattoli',
+      'sport',
+      'animali domestici',
+      'libri e riviste',
+      'accessori',
+      'motori',
 
- ];
- public function run():void {
-    foreach ($this->categories as $category) {
-        Category::create(['name'=>$category]);
-    }
- }
- 
+   ];
+   public function run(): void
+   {
+      foreach ($this->categories as $category) {
+         Category::create(['name' => $category]);
+      }
+
+
+      $users= [
+         [
+            "name" => "Linda",
+            "email" => "linda@mail.com",
+            "is_admin" => false,
+            "is_revisor" => false,
+            "email_verified_at" => now(),
+            "password" => Hash::make("linda1234"),
+            "created_at" => now(),
+            "updated_at" => now()
+         ],
+         [
+            "name" => "Pino",
+            "email" => "pino@mail.com",
+            "is_admin" => false,
+            "is_revisor" => false,
+            "email_verified_at" => now(),
+            "password" => Hash::make("pino1234"),
+            "created_at" => now(),
+            "updated_at" => now()
+         ],
+         [
+            "name" => "Pablo",
+            "email" => "pablo@mail.com",
+            "is_admin" => false,
+            "is_revisor" => true,
+            "email_verified_at" => now(),
+            "password" => Hash::make("pablo1234"),
+            "created_at" => now(),
+            "updated_at" => now(),
+
+         ],
+         [
+            "name" => "Arnold",
+            "email" => "arnold@mail.com",
+            "is_admin" => false,
+            "is_revisor" => false,
+            "email_verified_at" => now(),
+            "password" => Hash::make("arnold1234"),
+            "created_at" => now(),
+            "updated_at" => now()
+         ],
+         [
+            "name" => "Guohua",
+            "email" => "guohua@mail.com",
+            "is_admin" => false,
+            "is_revisor" => false,
+            "email_verified_at" => now(),
+            "password" => Hash::make("guohua1234"),
+            "created_at" => now(),
+            "updated_at" => now()
+         ],
+         [
+            "name" => "Michele",
+            "email" => "michele@mail.com",
+            "is_admin" => true,
+            "is_revisor" => false,
+            "email_verified_at" => now(),
+            "password" => Hash::make("michele1234"),
+            "created_at" => now(),
+            "updated_at" => now(),
+
+         ]
+
+      ];
+
+      foreach ($users as $user){
+         User::create($user);
+      }
+   }
 }
--- resources/views/components/shared/navbar.blade.php
+++ resources/views/components/shared/navbar.blade.php
@@ -1,31 +1,17 @@
 <nav class="navbar sticky-top navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
+  <div class="container">
     <a class="navbar-brand" href="{{route('home')}}">Presto</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav ms-auto pe-5">
+      <ul class="navbar-nav ms-auto ">
         <li class="nav-item">
           <a class="nav-link active" href="{{route('home')}}">Home
             <span class="visually-hidden">(current)</span>
           </a>
 
-          @auth
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profilo</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item text-light" href="#">Profilo utente</a>
-            <!-- <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a> -->
-            <div class="dropdown-divider"></div>
-            <form class="nav-link d-flex justify-content-center align-items-center" action="{{route('logout')}}" method="POST">
-              @csrf
-              <button type="submit" class="btn p-0 m-0 text-danger">Logout</button>
-            </form>
-            @endauth
-          </div>
-        </li>
+
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('article.index')}}">Tutti gli annunci</a>
@@ -52,17 +38,33 @@
         <li class="nav-item">
           <a class="nav-link" href="{{route('create.article')}}">Crea annuncio +</a>
         </li>
-        @endauth
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profilo</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item text-light" href="#">Profilo utente</a>
+            <!-- <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a> -->
+            <div class="dropdown-divider"></div>
+            <form class="nav-link d-flex justify-content-center align-items-center" action="{{route('logout')}}" method="POST">
+              @csrf
+              <button type="submit" class="btn p-0 m-0 text-danger">Logout</button>
+            </form>
 
-        @guest
+            @endauth
+
+            @guest
         <li class="nav-item">
           <a class="nav-link" href="{{route('login')}}">Login</a>
         </li>
         @endguest
 
 
-      </ul>
 
     </div>
+    </li>
+
+    </ul>
+
+  </div>
   </div>
 </nav>
