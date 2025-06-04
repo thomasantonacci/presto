692f157777ebdad858fec3309604121851e97581
Thomas Antonacci <collepasso@hotmail.it>
2025-06-04 19:10:52 +0200

    Iniziato User Story 6

---
+++ app/Jobs/ResizeImage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Spatie\Image\Enums\CropPosition;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ResizeImage implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    private $w, $h, $fileName, $path;
+    public function __construct($filePath, $w, $h)
+    {
+        $this->path = dirname($filePath);
+        $this->fileName = basename($filePath);
+        $this->w = $w;
+        $this->h = $h;
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $w = $this->w;
+        $h = $this->h;
+        $srcPath = storage_path() . '/app/public' . $this->path . '/' . $this->fileName;
+        $destPath = storage_path() . '/app/public' . $this->path . '/' . "crop_($w)x($h)_" . $this->fileName;
+        //
+        Image::load($srcPath)
+        ->crop($w,$h, CropPosition::Center)
+        ->save($destPath);
+    }
+}
