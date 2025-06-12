3a6761a203738d98229226e5dad514f68db9a750
ManuelDelVecchio <manuel.delvecchio@gmail.com>
2025-06-12 19:05:36 +0200

    completata user story #8

---
+++ app/Jobs/RemoveFaces.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Spatie\Image\Enums\Fit;
+use Spatie\Image\Enums\AlignPosition;
+use Illuminate\Queue\SerializesModels;
+use Spatie\Image\Image as SpatieImage;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+class RemoveFaces implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $artilce_image_id;
+    public function __construct($artilce_image_id)
+    {
+        $this->artilce_image_id = $artilce_image_id;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $i = Image::find($this->artilce_image_id);
+        if (!$i) {
+            return; 
+    }
+
+    $srcPath = storage_path('app/public/' . $i->path);
+    $image = file_get_contents($srcPath); 
+
+    putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google-credentials.json'));
+
+    $imageAnnotator = new ImageAnnotatorClient();
+    $response = $imageAnnotator->faceDetection($image);
+    $faces = $response->getFaceAnnotations();
+
+    foreach ($faces as $face) {
+            $vertices = $face->getBoundingPoly()->getVertices();
+
+            $bounds = [];
+            foreach ($vertices as $vertex) {
+                $bounds[] = [$vertex->getX(), $vertex->getY()];     
+        }
+
+        $w = $bounds[2][0] - $bounds[0][0];
+        $h = $bounds[2][1] - $bounds[0][1];
+
+        $image = SpatieImage::load($srcPath);+
+
+        $image->watermark(
+            base_path('resources/img/smile.png'),
+            AlignPosition::TopLeft,
+            paddingX:$bounds[0][0],
+            paddingY:$bounds[0][1],
+            width: $w,
+            height: $h,
+            fit: Fit::Stretch
+        );
+        $image->save($srcPath);
+    }
+    $imageAnnotator->close();
+}
+}
